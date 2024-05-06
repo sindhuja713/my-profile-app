d00b4f31e659c94b10faf1f37ac7e3627d930ab4
sindhuja713 <157420890+sindhuja713@users.noreply.github.com>
2024-05-06 14:39:19 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.module.css;"
+
+function App () {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+export default App;
+
---
+++ src/components/profile/UI/card/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./Card.module.css";
+
+const Card = () => {
+  return (
+    <div>
+        <h1>Card</h1>
+    </div>
+  );
+};
+
+export default Card;
